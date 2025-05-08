65d5e6a3aa7eb9659c91680d5475a903c44979d9
Twisha Bansal <58483338+twishabansal@users.noreply.github.com>
2025-05-08 21:55:30 +0530

    lint

--- packages/toolbox-core/__tests__/test_client.js
+++ packages/toolbox-core/__tests__/test_client.js
@@ -1,9 +1,9 @@
 import { ToolboxClient } from "../build/toolbox_core/client.js";
 
-const client = new ToolboxClient('https://some_base_url');
+const client = new ToolboxClient("https://some_base_url");
 
-describe('getToolResponse', () => {
-  test('Should return a specific value based on inputs', async () => {
+describe("getToolResponse", () => {
+  test("Should return a specific value based on inputs", async () => {
     const response = await client.getToolResponse(3, 4);
     expect(response).toBe(11);
   });
