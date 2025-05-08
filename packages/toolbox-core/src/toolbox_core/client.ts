626cdd4e86d9b9f3424f3e65a70646bf317a398f
Twisha Bansal <58483338+twishabansal@users.noreply.github.com>
2025-05-08 22:49:13 +0530

    lint

--- packages/toolbox-core/src/toolbox_core/client.ts
+++ packages/toolbox-core/src/toolbox_core/client.ts
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ToolboxTool } from "./tool";
+import { ToolboxTool } from './tool';
+
+// Just testing if lint workflow is working
 
 class ToolboxClient {
   /** @private */ _baseUrl;
@@ -20,8 +22,10 @@ class ToolboxClient {
   /**
    * @param {string} url - The base URL for the Toolbox service API.
    */
-  constructor(url: string) {
-    this._baseUrl = url;
+  constructor(
+      url: string,
+  ) {
+      this._baseUrl = url;
   }
 
   /**
@@ -30,9 +34,9 @@ class ToolboxClient {
    * @returns {int} - Mock API response.
    */
   async getToolResponse(num1: number, num2: number) {
-    const tool = ToolboxTool("tool1");
-    const response = await tool({ a: num1, b: num2 });
-    return response;
+      const tool = ToolboxTool('tool1');
+      const response = await tool({'a': num1, 'b': num2});
+      return response;
   }
 }
 
