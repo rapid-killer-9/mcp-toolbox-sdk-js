4492de98afdcbcac531f0dfa8ed5560e7ffae40a
Twisha Bansal <58483338+twishabansal@users.noreply.github.com>
2025-05-08 22:07:01 +0530

    lint

--- packages/toolbox-core/src/toolbox_core/client.ts
+++ packages/toolbox-core/src/toolbox_core/client.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ToolboxTool } from './tool';
+import { ToolboxTool } from "./tool";
 
 class ToolboxClient {
   /** @private */ _baseUrl;
